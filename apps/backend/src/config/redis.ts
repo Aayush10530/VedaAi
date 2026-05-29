55b06f0d7a6bfc12c651267a2a8fbbe0609e3900
Aayush Mishra <aayushmishra01530@gmail.com>
2026-05-29 13:43:45 +0530

    add tls support

--- apps/backend/src/config/redis.ts
+++ apps/backend/src/config/redis.ts
@@ -1,6 +1,9 @@
 import Redis from 'ioredis';
 import { env } from './env';
 
+const redisUrl = new URL(env.REDIS_URL);
+
 export const redis = new Redis(env.REDIS_URL, {
   maxRetriesPerRequest: null,
+  ...(redisUrl.protocol === 'rediss:' ? { tls: {} } : {}),
 });
